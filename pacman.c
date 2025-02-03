282149be5c525f7893a2e8b18d4fc6218b1f9ac3
MobinaKhoeini <mobina.khoeini12@gmail.com>
2025-02-03 03:44:07 +0330

    Update pacman.c

--- pacman.c
+++ pacman.c
@@ -1,24 +1,25 @@
-#include <conio.h> 
-#include <stdio.h> 
-#include <stdlib.h> 
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <windows.h>
 
-#define WIDTH 40 
-#define HEIGHT 20 
-#define PACMAN 'C' 
-#define WALL '#' 
-#define FOOD '.' 
-#define EMPTY ' ' 
-#define DEMON 'X' 
+#define WIDTH 40
+#define HEIGHT 20
+#define PACMAN 'C'
+#define WALL '#'
+#define FOOD '.'
+#define EMPTY ' '
+#define DEMON 'X'
 
-int res = 0; 
-int score = 0; 
-int pacman_x, pacman_y; 
-int food = 0; 
-int curr = 0;  
+int res = 0;
+int score = 0;
+int pacman_x, pacman_y;
+int food = 0;
+int curr = 0;
 
-typedef struct {
+typedef struct
+{
     int x;
     int y;
     char type;
@@ -26,135 +27,172 @@ typedef struct {
 
 element packman[HEIGHT][WIDTH];
 
-void initialize() { 
-    for (int i = 0; i < HEIGHT; i++) { 
-        for (int j = 0; j < WIDTH; j++) { 
-            if (i == 0||j == (WIDTH - 1) || j == 0 || i == (HEIGHT - 1)) { 
-                packman[i][j].type = WALL;
-            }
-             else {
-                packman[i][j].type = EMPTY;
-            }
-            
-        }
-    }
-
-    int count = 50; 
-    while (count != 0) { 
-        int i = rand() % HEIGHT; 
-        int j = rand() % WIDTH; 
-        if (packman[i][j].type != WALL && packman[i][j].type != PACMAN) { 
-            packman[i][j].type = WALL;  
-            count--; 
-        } 
-    }
-
-    int val = 5; 
-    while (val--) { 
-        int row = rand() % HEIGHT; 
-        for (int j = 3; j < WIDTH - 3; j++) { 
-            if (packman[row][j].type != WALL && packman[row][j].type != PACMAN) {
-                packman[row][j].type = WALL;
-            } 
-        } 
-    } 
-
-    count = 10; 
-    while (count != 0) { 
-        int i = rand() % HEIGHT; 
-        int j = rand() % WIDTH; 
-
-        if (packman[i][j].type != WALL && packman[i][j].type != PACMAN) { 
-            packman[i][j].type = DEMON;
-            count--; 
-        } 
-    } 
-
-    pacman_x = HEIGHT / 2; 
-    pacman_y = WIDTH / 2; 
-    packman[pacman_x][pacman_y].type = PACMAN;
-
-    for (int i = 0; i < HEIGHT; i++) { 
-        for (int j = 0; j < WIDTH; j++) { 
-            if (i % 2 == 0 && j % 2 == 0 && packman[i][j].type == EMPTY) { 
-                packman[i][j].type = FOOD;
-                food++; 
-            } 
-        } 
-    } 
-} 
-
-void draw() { 
-    system("cls"); 
-    for (int i = 0; i < HEIGHT; i++) { 
-        for (int j = 0; j < WIDTH; j++) { 
-            printf("%c", packman[i][j].type);
-        } 
-        printf("\n"); 
-    } 
-    printf("Score: %d\n", score); 
-} 
-
-void move(int movex, int movey) { 
-    int x = pacman_x + movex; 
-    int y = pacman_y + movey; 
-
-    if (packman[x][y].type != WALL) { 
-        if (packman[x][y].type == FOOD) { 
-            score++; 
-            food--; 
-            curr++; 
-            if (food == 0) { 
-                res = 2; 
-                return; 
-            } 
-        } else if (packman[x][y].type == DEMON) { 
-            res = 1; 
-            return;
-        } 
-        packman[pacman_x][pacman_y].type = EMPTY;
-        pacman_x = x; 
-        pacman_y = y; 
-        packman[pacman_x][pacman_y].type = PACMAN;
-    } 
-} 
-void computerMode()
+void initialize()
 {
-    int food_x = -1, food_y = -1;
     for (int i = 0; i < HEIGHT; i++)
     {
         for (int j = 0; j < WIDTH; j++)
         {
-            if (packman[i][j].type == FOOD)
+            if (i == 0 || j == (WIDTH - 1) || j == 0 || i == (HEIGHT - 1))
             {
-                food_x = i;
-                food_y = j;
-                break;
+                packman[i][j].type = WALL;
+            }
+            else
+            {
+                packman[i][j].type = EMPTY;
             }
         }
-        if (food_x != -1)
-            break;
     }
-    if (food_x == -1)
-        return;
-    if (pacman_x < food_x)
+
+    int count = 50;
+    while (count != 0)
     {
-        move(1, 0);
+        int i = rand() % HEIGHT;
+        int j = rand() % WIDTH;
+        if (packman[i][j].type != WALL && packman[i][j].type != PACMAN)
+        {
+            packman[i][j].type = WALL;
+            count--;
+        }
     }
-    else if (pacman_x > food_x)
+
+    int val = 5;
+    while (val--)
     {
-        move(-1, 0);
+        int row = rand() % HEIGHT;
+        for (int j = 3; j < WIDTH - 3; j++)
+        {
+            if (packman[row][j].type != WALL && packman[row][j].type != PACMAN)
+            {
+                packman[row][j].type = WALL;
+            }
+        }
     }
-    else if (pacman_y < food_y)
+
+    count = 10;
+    while (count != 0)
     {
-        move(0, 1);
+        int i = rand() % HEIGHT;
+        int j = rand() % WIDTH;
+
+        if (packman[i][j].type != WALL && packman[i][j].type != PACMAN)
+        {
+            packman[i][j].type = DEMON;
+            count--;
+        }
     }
-    else if (pacman_y > food_y)
+
+    pacman_x = HEIGHT / 2;
+    pacman_y = WIDTH / 2;
+    packman[pacman_x][pacman_y].type = PACMAN;
+
+    for (int i = 0; i < HEIGHT; i++)
     {
-        move(0, -1);
+        for (int j = 0; j < WIDTH; j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0 && packman[i][j].type == EMPTY)
+            {
+                packman[i][j].type = FOOD;
+                food++;
+            }
+        }
     }
 }
 
+void draw()
+{
+    system("cls");
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        for (int j = 0; j < WIDTH; j++)
+        {
+            printf("%c", packman[i][j].type);
+        }
+        printf("\n");
+    }
+    printf("Score: %d\n", score);
+}
+
+void move(int move_x, int move_y)
+{
+    int x = pacman_x + move_x;
+    int y = pacman_y + move_y;
+
+    if (packman[x][y].type != WALL)
+    {
+        if (packman[x][y].type == FOOD)
+        {
+            score++;
+            food--;
+            curr++;
+            if (food == 0)
+            {
+                res = 2;
+                return;
+            }
+        }
+        else if (packman[x][y].type == DEMON)
+        {
+            res = 1;
+            return;
+        }
+        packman[pacman_x][pacman_y].type = EMPTY;
+        pacman_x = x;
+        pacman_y = y;
+        packman[pacman_x][pacman_y].type = PACMAN;
+    }
+}
+void computerMode(int totalfood)
+{
+    int move_x=0;
+    int move_y=0;
+    int direction=rand()%4;
+    switch (direction)
+    {
+        case 0:
+        {
+            move_x=-1; move_y=0;
+        } break;
+        case 1:
+        {
+            move_x=1; move_y=0;
+        } break;
+        case 2:
+        {
+            move_x=0; move_y=-1;
+        } break;
+        case 3:
+        {
+            move_x=0; move_y=1;
+        } break;
+    }
+        move(move_x,move_y);
+        /*
+        draw();
+
+                printf("Total Food count: %d\n", totalFood);
+                printf("Total Food eaten: %d\n", curr);
+
+                if (res == 1)
+                {
+                    // Clear screen
+                    system("cls");
+                    printf("Game Over! Dead by Demon\n Your Score: "
+                           "%d\n",
+                           score);
+                    return;
+                }
+
+                if (res == 2)
+                {
+                    // Clear screen
+                    system("cls");
+                    printf("You Win! \n Your Score: %d\n", score);
+                    return;
+                }
+                */
+
+}
 int main()
 {
     srand(time(0)); // Initialize random seed
@@ -172,7 +210,6 @@ int main()
         printf("Exit Game! ");
         return 1;
     }
-        {
     while (1)
     {
         printf("please choose your required option:\n");
@@ -181,89 +218,94 @@ int main()
         int Choice;
         scanf("%d", &Choice);
         switch (Choice)
-    {
-        case 1:
-        while(1)
         {
-             draw();
-            printf("Total Food count: %d\n", totalFood);
-            printf("Total Food eaten: %d\n", curr);
-             if (res == 1)
+        case 1:
+        {
+            while (1)
             {
-                // Clear screen
-                system("cls");
-                printf("Game Over! Dead by Demon\n Your Score: "
-                       "%d\n",
-                       score);
-                return 1;
-            }
+                
+                draw();
+                printf("Total Food count: %d\n", totalFood);
+                printf("Total Food eaten: %d\n", curr);
 
-            if (res == 2)
-            {
-                // Clear screen
-                system("cls");
-                printf("You Win! \n Your Score: %d\n", score);
-                return 1;
+                if (res == 1)
+                {
+                    // Clear screen
+                    system("cls");
+                    printf("Game Over! Dead by Demon\n Your Score: "
+                           "%d\n",
+                           score);
+                    return 1;
+                }
+
+                if (res == 2)
+                {
+                    // Clear screen
+                    system("cls");
+                    printf("You Win! \n Your Score: %d\n", score);
+                    return 1;
+                }
+                
+                computerMode(totalFood);
+                Sleep(1000);
             }
-            computerMode();
         }
         break;
         case 2:
-        while (1)
         {
-            draw();
-            printf("Total Food count: %d\n", totalFood);
-            printf("Total Food eaten: %d\n", curr);   
-            if (res == 1)
+            while (1)
             {
-                // Clear screen
-                system("cls");
-                printf("Game Over! Dead by Demon\n Your Score: "
-                       "%d\n",
-                       score);
-                return 1;
-            }
+                draw();
+                printf("Total Food count: %d\n", totalFood);
+                printf("Total Food eaten: %d\n", curr);
+                if (res == 1)
+                {
+                    // Clear screen
+                    system("cls");
+                    printf("Game Over! Dead by Demon\n Your Score: "
+                           "%d\n",
+                           score);
+                    return 1;
+                }
 
-            if (res == 2)
-            {
-                // Clear screen
-                system("cls");
-                printf("You Win! \n Your Score: %d\n", score);
-                return 1;
-            }
+                if (res == 2)
+                {
+                    // Clear screen
+                    system("cls");
+                    printf("You Win! \n Your Score: %d\n", score);
+                    return 1;
+                }
 
-        // Taking the Input from the user
-        ch = getch();
-        // Moving According to the
-        // input character
-        switch (ch)
-        {
-        case 'w':
-            move(0, -1);
-            break;
-        case 's':
-            move(0, 1);
-            break;
-        case 'a':
-            move(-1, 0);
-            break;
-        case 'd':
-            move(1, 0);
-            break;
-        case 'q':
-            printf("Game Over! Your Score: %d\n", score);
-            return 0;
-        }
+                // Taking the Input from the user
+                ch = getch();
+                // Moving According to the
+                // input character
+                switch (ch)
+                {
+                case 'w':
+                    move(-1, 0);
+                    break;
+                case 's':
+                    move(1, 0);
+                    break;
+                case 'a':
+                    move(0, -1);
+                    break;
+                case 'd':
+                    move(0, 1);
+                    break;
+                case 'q':
+                    printf("Game Over! Your Score: %d\n", score);
+                    return 0;
+                }
+            }
         }
         break;
         default:
-        printf("invalid choice!\n");
-        break;
-    }
+            printf("invalid choice!\n");
+            break;
+        }
     }
 
     return 0;
-}
-
-  
 }
